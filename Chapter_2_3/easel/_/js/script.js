0430fcd92b957a04c16571f1128c3be1be5d8242
Howlinmoon <github@strifecrafter.com>
2015-07-28 18:51:27 -0400

    finished up working with gradients - radial (circular) gradients

--- Chapter_2_3/easel/_/js/script.js
+++ Chapter_2_3/easel/_/js/script.js
@@ -9,8 +9,11 @@ function init() {
     // or programatically (4th octet is an alpha channel)
     // shape.graphics.beginFill(createjs.Graphics.getRGB(162, 216, 255, .5));
     
-    // or you can do it as a gradient
-    shape.graphics.beginLinearGradientFill(["yellow", "red", "blue"], [0, .5, 1], 50, 50, 50, 150);
+    // or you can do it as a gradient (linear first)
+    // shape.graphics.beginLinearGradientFill(["yellow", "red", "blue"], [0, .5, 1], 50, 50, 50, 150);
+    // now, this is how you do a RadialGradient - circles
+    // circle0 X, circle0 Y, circle0 Radius, circle1 X, circle1 Y, circle1 Radius
+    shape.graphics.beginRadialGradientFill(["yellow", "blue"], [0, 1], 100, 100, 0, 100, 100, 100);
     shape.graphics.rect(50, 50, 100, 100);
     
     // create a stage
