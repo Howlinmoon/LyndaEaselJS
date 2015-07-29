22a9b95eb2dc5c39bbaa7a448101d22ffb25c0b8
Jim Veneskey <github@strifecrafter.com>
2015-07-29 12:28:59 -0400

    Compacted about 5 lines of code into about 1/5 the amount of space

--- Chapter_2_6/easel/_/js/script.js
+++ Chapter_2_6/easel/_/js/script.js
@@ -3,13 +3,23 @@ function init() {
 	stage = new createjs.Stage(canvas),
 	shape = new createjs.Shape();
 	
-	shape.graphics.beginStroke("purple");
-	shape.graphics.setStrokeStyle(10, "round", "round");
-	shape.graphics.moveTo(50, 50);
-	shape.graphics.lineTo(250, 250);
-	shape.graphics.lineTo(50, 250);
-	shape.graphics.closePath();
+//	shape.graphics.beginStroke("purple");
+//	shape.graphics.setStrokeStyle(10, "round", "round");
+// combining the two preceding lines into one
+    
+//    shape.graphics.beginStroke("blue").setStrokeStyle(10, "round", "round");
+//	shape.graphics.moveTo(50, 50);
+//	shape.graphics.lineTo(250, 250);
+//	shape.graphics.lineTo(50, 250);
+//	shape.graphics.closePath();
 
+// combine ALL of the lines into one long line
+//shape.graphics.beginStroke("red").setStrokeStyle(10, "round", "round").moveTo(50, 50).lineTo(250, 250).lineTo(50, 250).closePath();
+    
+// Next - we can abbreviate most commands to a single letter or two
+shape.graphics.s("yellow").ss(10, "round", "round").mt(50, 50).lt(250, 250).lt(50, 250).cp();
+    
+    
 	stage.addChild(shape);
 	stage.update();
 }
