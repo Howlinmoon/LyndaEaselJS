870e68f44223175a816cc68040c59806f06e3297
Jim Veneskey <github@strifecrafter.com>
2015-07-30 06:41:19 -0400

    starting the final project

---
+++ Chapter_4_2/easel/_/js/script.js
@@ -0,0 +1,28 @@
+function init() {
+	var canvas = document.getElementById("easel"),
+	stage = new createjs.Stage(canvas),
+	centerX = canvas.width/2,
+	centerY = canvas.height/2,
+	ss = new createjs.SpriteSheet({
+		animations: {
+			fly: [0, 15],
+			explode: [16, 20, "fly"] },
+		images: ["images/shipsprites.png"],
+		frames: {
+			regX: 50,
+			regY: 50,
+			height: 100,
+			width: 100
+		}
+	}),
+	ship = new createjs.BitmapAnimation(ss);
+	ship.x = centerX;
+	ship.y = centerY;
+	ship.gotoAndPlay("fly");
+
+	stage.addChild(ship);	
+	createjs.Ticker.setFPS(30);
+	createjs.Ticker.addListener(function() {
+		stage.update();
+	});
+}
